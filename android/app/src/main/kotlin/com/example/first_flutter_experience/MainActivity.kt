5e3f0a4cbaa37b8eb8a83206aaa8b6420e04ce23
Bjoern Ludwig <bgithub@kletterni3d.de>
2024-11-10 16:00:53 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/first_flutter_experience/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.first_flutter_experience
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:streak_your_life/main.dart';
+
+void main() {
+  testWidgets('Placeholder Test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+  });
+}
