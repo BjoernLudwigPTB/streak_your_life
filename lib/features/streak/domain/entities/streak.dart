486351486c163574a51d3e4402303db2375ebb30
Bjoern Ludwig <bgithub@kletterni3d.de>
2024-11-28 23:03:22 +0100

    refactor: improve RecurrenceType to be able to represent more complex semantics

--- lib/features/streak/domain/entities/streak.dart
+++ lib/features/streak/domain/entities/streak.dart
@@ -1,9 +1,32 @@
 /// Enum representing the recurrence type of a streak.
+///
+/// Each enum value maps to a string representation used in JSON.
 enum RecurrenceType {
-  daily,
-  weekly,
-  monthly,
-  yearly,
+  daily("1d"),
+  weekly("1w"),
+  monthly("1m"),
+  yearly("1y");
+
+  /// The string representation of the recurrence type.
+  final String code;
+
+  /// Private constructor for mapping string codes to enum values.
+  const RecurrenceType(this.code);
+
+  /// Converts a string code to the corresponding [RecurrenceType].
+  ///
+  /// Returns `null` if the code is invalid.
+  static RecurrenceType? fromCode(String code) {
+    for (final type in RecurrenceType.values) {
+      if (type.code == code) {
+        return type;
+      }
+    }
+    return null; // Return null if no match is found
+  }
+
+  /// Returns the string code of the current [RecurrenceType].
+  String toCode() => code;
 }
 
 /// A data class to store information about streaks.
