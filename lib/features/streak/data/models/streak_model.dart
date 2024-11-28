2b2e21d98bb8b38d636ee8c42a3a2428ba954eff
Bjoern Ludwig <bgithub@kletterni3d.de>
2024-11-28 23:06:00 +0100

    refactor: introduce StreakModel with tests

--- lib/features/streak/data/models/streak_model.dart
+++ lib/features/streak/data/models/streak_model.dart
@@ -1,20 +1,59 @@
-import '../../domain/entities/streak.dart';
+import '../../domain/entities/streak.dart' show RecurrenceType, Streak;
 
-/// A data model class for streaks that extends the [Streak] entity.
+/// A model class for handling streak data in the data layer.
 ///
-/// The [StreakModel] class represents the implementation of a streak
-/// in the data layer.
+/// The [StreakModel] class extends the [Streak] entity and provides
+/// methods to convert between JSON and the internal [Streak] representation.
 class StreakModel extends Streak {
-  /// Creates a [StreakModel] instance with the specified properties.
+  /// Creates a [StreakModel] instance with the given properties.
   ///
-  /// - [name]: The name of the streak (e.g., "Daily Exercise").
-  /// - [recurrence]: Specifies the recurrence pattern of the streak
-  ///   (e.g., daily, weekly).
-  /// - [lastOccurrence]: Optionally specifies the last occurrence date
-  ///   of the streak. If not provided, it defaults to `null`.
-  StreakModel({
+  /// [name]: The name of the streak.
+  /// [recurrence]: The type of recurrence, represented by [RecurrenceType].
+  /// [lastOccurrence]: (Optional) The date and time of the last occurrence.
+  const StreakModel({
     required super.name,
     required super.recurrence,
     super.lastOccurrence,
   });
+
+  /// Creates a [StreakModel] from a JSON object.
+  ///
+  /// [json]: A Map containing the fields `name`, `recurrence`, and `lastOccurrence`.
+  /// - `name` is a [String].
+  /// - `recurrence` is a [String] corresponding to a [RecurrenceType].
+  /// - `lastOccurrence` is an ISO 8601 formatted [String] or `null`.
+  ///
+  /// Always returns a valid [StreakModel], using `RecurrenceType.daily` as
+  /// the default if the `recurrence` code is invalid.
+  factory StreakModel.fromJSON(Map<String, dynamic> json) {
+    final recurrenceCode = json['recurrence'] as String;
+    final recurrenceType =
+        RecurrenceType.fromCode(recurrenceCode) ?? RecurrenceType.daily;
+
+    final lastOccurrence = json['lastOccurrence'] != null
+        ? DateTime.parse(json['lastOccurrence'] as String)
+        : null;
+
+    return StreakModel(
+      name: json['name'] as String,
+      recurrence: recurrenceType,
+      lastOccurrence: lastOccurrence,
+    );
+  }
+
+  /// Converts the [StreakModel] into a JSON-compatible Map.
+  ///
+  /// The resulting Map contains:
+  /// - `name`: A [String] representing the name of the streak.
+  /// - `recurrence`: A [String] representing the recurrence type.
+  /// - `lastOccurrence`: An ISO 8601 formatted [String] or `null`.
+  ///
+  /// Returns a JSON-compatible Map.
+  Map<String, dynamic> toJSON() {
+    return {
+      'name': name,
+      'recurrence': recurrence.toCode(),
+      'lastOccurrence': lastOccurrence?.toIso8601String(),
+    };
+  }
 }
--- test/features/streak/data/models/streak_model_test.dart
+++ test/features/streak/data/models/streak_model_test.dart
@@ -1,10 +1,18 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:streak_your_life/features/streak/data/models/streak_model.dart';
-import 'package:streak_your_life/features/streak/domain/entities/streak.dart';
+import 'dart:convert' show json;
+
+import 'package:flutter_test/flutter_test.dart' show expect, group, isA, test;
+import 'package:streak_your_life/features/streak/data/models/streak_model.dart'
+    show StreakModel;
+import 'package:streak_your_life/features/streak/domain/entities/streak.dart'
+    show RecurrenceType, Streak;
+
+import '../../../../fixtures/fixture_reader.dart' show fixture;
 
 void main() {
-  final tStreakModel =
-      StreakModel(name: "test streak", recurrence: RecurrenceType.daily);
+  final tStreakModel = StreakModel(
+      name: "test streak",
+      recurrence: RecurrenceType.daily,
+      lastOccurrence: DateTime.parse("2024-11-25T09:07:32.770"));
 
   test(
     'should be a subclass of the Streak entity',
@@ -13,4 +21,31 @@ void main() {
       expect(tStreakModel, isA<Streak>());
     },
   );
+
+  group('fromJSON', () {
+    test(
+      'should return a valid model when the JSON is valid',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = json.decode(fixture('streak.json'));
+        // act
+        final result = StreakModel.fromJSON(jsonMap);
+        // assert
+        expect(result, tStreakModel);
+      },
+    );
+
+    test(
+      'should return the correct JSON after back and forth conversion',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = json.decode(fixture('streak.json'));
+        // act
+        final model = StreakModel.fromJSON(jsonMap);
+        final result = model.toJSON();
+        // assert
+        expect(result, jsonMap);
+      },
+    );
+  });
 }
